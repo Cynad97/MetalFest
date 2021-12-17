d88f9e97827381995ad9af4d5a69bf4708f07e86
Cynad97 <cynad97@gmail.com>
2021-12-17 19:38:58 -0300

    imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const {series, src, dest, watch} = require('gulp'); //require importa las dependencias que instalamos con npm install
 const sass = require('gulp-sass')(require('sass')); //Cuando no hay llaves es que solo hay una funcion.
+const imagemin = require('gulp-imagemin');
 
 //Funcion que compila SASS
 
@@ -19,10 +20,17 @@ function minificarcss() {
         .pipe(dest('./build/css'))
 }
 
+function images() {
+    return src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(dest('./build/img'))
+}
+
 //cada vez que hay un cambio en el archivo se ejecuta la funcion css.
 function watchFiles(){
     watch('src/scss/**/*.scss',css); //* es = La carpeta actual - ** = Todos los archivos con esa extension.
 }
 exports.css = css;
 exports.minificarcss = minificarcss;
+exports.images = images;
 exports.watchFiles = watchFiles;
